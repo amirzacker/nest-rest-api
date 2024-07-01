c355082e09c8d0cf1d1986e9d1a531c179f9a759
amir_zaky <abgurene4@gmail.com>
2024-07-01 10:27:19 +0200

    basic: rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'amir',
-      password: 'amircentre',
-      database: 'shopping_list_db',
+      host: process.env.HOST_DB || 'localhost',
+      port: process.env.PORT ? parseInt(process.env.PORT_DB) : 3306,
+      username: process.env.USERNAME_DB || 'root',
+      password: process.env.PASSWORD_DB || '',
+      database: process.env.DB_NAME || 'test',
       entities: [User],
       synchronize: true, // Use only in development!
     }),
